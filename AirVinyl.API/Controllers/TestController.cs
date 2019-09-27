fa6c1cf7a40001ce641bf1a508a11125a03fdb7c
Michal Delura <michal.delura@mountainwarehouse.com>
2019-09-27 12:33:41 +0200

    Add OData

---
+++ AirVinyl.API/Controllers/TestController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace AirVinyl.API.Controllers
+{
+    public class TestController : ApiController
+    {
+        [HttpGet]
+        public IHttpActionResult Index()
+        {
+            return Ok();
+        }
+
+    }
+}
